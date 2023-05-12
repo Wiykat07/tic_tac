a8d001e20199a72dcb0ce374fa2d412666ab0a4f
Wiykat07 <katherinewiykovics@gmail.com>
2023-05-12 11:45:36 -0400

    Removed unnecessary consumer

--- lib/Widgets/scaffolds.dart
+++ lib/Widgets/scaffolds.dart
@@ -22,38 +22,36 @@ class LandscapeScaffold extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<GameProvider>(builder: (context, game, child) {
-      return Scaffold(
-          appBar: AppBar(
-              leading: BackButton(
-                onPressed: () {
-                  game.emptyBoard();
-                  Navigator.of(context).pop();
-                },
-              ),
-              title: Text(title)),
-          body: Stack(children: [
-            SizedBox(
-              height: height,
-              width: width,
-              child: CustomPaint(
-                  painter: BoardMaker(
-                      Provider.of<Preferences>(context, listen: false)
-                          .secondary)),
+    return Scaffold(
+        appBar: AppBar(
+            leading: BackButton(
+              onPressed: () {
+                Provider.of<GameProvider>(context, listen: false).emptyBoard();
+                Navigator.of(context).pop();
+              },
             ),
-            AlignPositioned(
-                alignment: Alignment.topLeft,
-                dx: width * .2,
-                dy: height * .07,
-                child: Column(children: [
-                  buildRows(height, width, false, [0, 1, 2], inter),
-                  SizedBox(height: height * .009, width: width * .012),
-                  buildRows(height, width, false, [3, 4, 5], inter),
-                  SizedBox(height: height * .03, width: width * .012),
-                  buildRows(height, width, false, [6, 7, 8], inter),
-                ]))
-          ]));
-    });
+            title: Text(title)),
+        body: Stack(children: [
+          SizedBox(
+            height: height,
+            width: width,
+            child: CustomPaint(
+                painter: BoardMaker(
+                    Provider.of<Preferences>(context, listen: false)
+                        .secondary)),
+          ),
+          AlignPositioned(
+              alignment: Alignment.topLeft,
+              dx: width * .2,
+              dy: height * .07,
+              child: Column(children: [
+                buildRows(height, width, false, [0, 1, 2], inter),
+                SizedBox(height: height * .009, width: width * .012),
+                buildRows(height, width, false, [3, 4, 5], inter),
+                SizedBox(height: height * .03, width: width * .012),
+                buildRows(height, width, false, [6, 7, 8], inter),
+              ]))
+        ]));
   }
 }
 
@@ -72,42 +70,40 @@ class PortraitScaffold extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<GameProvider>(builder: (context, game, child) {
-      return Scaffold(
-        appBar: AppBar(
-            leading: BackButton(
-              onPressed: () {
-                game.emptyBoard();
-                Navigator.of(context).pop();
-              },
-            ),
-            title: Text(title)),
-        body: Center(
-            child: Stack(
-          children: [
-            SizedBox(
-              height: height,
-              width: width,
-              child: CustomPaint(
-                  painter: BoardMaker(
-                      Provider.of<Preferences>(context, listen: false)
-                          .secondary)),
-            ),
-            AlignPositioned(
-              alignment: Alignment.topLeft,
-              dx: width * .095,
-              dy: height * .18,
-              child: Column(children: [
-                buildRows(height, width, true, [0, 1, 2], inter),
-                SizedBox(height: height * .007, width: width * .04),
-                buildRows(height, width, true, [3, 4, 5], inter),
-                SizedBox(height: height * .007, width: width * .04),
-                buildRows(height, width, true, [6, 7, 8], inter),
-              ]),
-            )
-          ],
-        )),
-      );
-    });
+    return Scaffold(
+      appBar: AppBar(
+          leading: BackButton(
+            onPressed: () {
+              Provider.of<GameProvider>(context, listen: false).emptyBoard();
+              Navigator.of(context).pop();
+            },
+          ),
+          title: Text(title)),
+      body: Center(
+          child: Stack(
+        children: [
+          SizedBox(
+            height: height,
+            width: width,
+            child: CustomPaint(
+                painter: BoardMaker(
+                    Provider.of<Preferences>(context, listen: false)
+                        .secondary)),
+          ),
+          AlignPositioned(
+            alignment: Alignment.topLeft,
+            dx: width * .095,
+            dy: height * .18,
+            child: Column(children: [
+              buildRows(height, width, true, [0, 1, 2], inter),
+              SizedBox(height: height * .007, width: width * .04),
+              buildRows(height, width, true, [3, 4, 5], inter),
+              SizedBox(height: height * .007, width: width * .04),
+              buildRows(height, width, true, [6, 7, 8], inter),
+            ]),
+          )
+        ],
+      )),
+    );
   }
 }
