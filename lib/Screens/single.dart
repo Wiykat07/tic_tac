8b1875e214915df8763094c55c70371d2e486c23
Wiykat07 <katherinewiykovics@gmail.com>
2023-05-15 18:47:56 -0400

    Fixed issue where single player page would overflow in landscape mode when the keyboard appeared.

--- lib/Screens/single.dart
+++ lib/Screens/single.dart
@@ -40,60 +40,62 @@ class _SingleState extends State<Single> {
 
     return Scaffold(
       appBar: AppBar(title: const Text('Enter your name')),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              width: 200,
-              child: TextField(
-                decoration:
-                    const InputDecoration(labelText: 'Player One\'s name:'),
-                keyboardType: TextInputType.name,
-                controller: player1Controller,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.allow(RegExp(r'[a-zA-z]')),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                width: 200,
+                child: TextField(
+                  decoration:
+                      const InputDecoration(labelText: 'Player One\'s name:'),
+                  keyboardType: TextInputType.name,
+                  controller: player1Controller,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.allow(RegExp(r'[a-zA-z]')),
+                  ],
+                ),
+              ),
+              SizedBox.fromSize(
+                size: Size.fromHeight(height * .05),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Difficulty: '),
+                  DropdownButton(
+                      value: diff,
+                      icon: const Icon(Icons.keyboard_arrow_down),
+                      items: difficulty.map((String diff) {
+                        return DropdownMenuItem(value: diff, child: Text(diff));
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          diff = newValue!;
+                          ai = difficult(diff);
+                        });
+                      }),
                 ],
               ),
-            ),
-            SizedBox.fromSize(
-              size: Size.fromHeight(height * .05),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Difficulty: '),
-                DropdownButton(
-                    value: diff,
-                    icon: const Icon(Icons.keyboard_arrow_down),
-                    items: difficulty.map((String diff) {
-                      return DropdownMenuItem(value: diff, child: Text(diff));
-                    }).toList(),
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        diff = newValue!;
-                        ai = difficult(diff);
-                      });
-                    }),
-              ],
-            ),
-            Text(description),
-            SizedBox.fromSize(
-              size: Size.fromHeight(height * .05),
-            ),
-            OutlinedButton(
-                onPressed: () {
-                  names.add(player1Controller.text);
-                  names.add('Computer');
-                  Provider.of<GameProvider>(context, listen: false)
-                      .addPlayer(true, names[1], PlayerNumber.ai);
-                  Provider.of<GameProvider>(context, listen: false)
-                      .addPlayer(false, names[0], PlayerNumber.player1);
-                  Provider.of<GameProvider>(context, listen: false)
-                      .switchTurns(false);
-                  Navigator.pushNamed(context, '/board', arguments: ai);
-                },
-                child: const Text('Let\'s play!'))
-          ],
+              Text(description),
+              SizedBox.fromSize(
+                size: Size.fromHeight(height * .05),
+              ),
+              OutlinedButton(
+                  onPressed: () {
+                    names.add(player1Controller.text);
+                    names.add('Computer');
+                    Provider.of<GameProvider>(context, listen: false)
+                        .addPlayer(true, names[1], PlayerNumber.ai);
+                    Provider.of<GameProvider>(context, listen: false)
+                        .addPlayer(false, names[0], PlayerNumber.player1);
+                    Provider.of<GameProvider>(context, listen: false)
+                        .switchTurns(false);
+                    Navigator.pushNamed(context, '/board', arguments: ai);
+                  },
+                  child: const Text('Let\'s play!'))
+            ],
+          ),
         ),
       ),
     );
